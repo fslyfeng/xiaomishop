e93c80ed747f3cb20f1df8cd6eb27c2c09242556
netren <fslyfeng@gmail.com>
2022-01-07 23:46:35 +0800

    轮播图组件开发

--- App.vue
+++ App.vue
@@ -13,15 +13,20 @@ export default {
 </script>
 
 <style>
-/*每个页面公共css */
-/* 官方ui库 */
-@import '/common/uni.css';
-/* 第三方动画库 */
-@import '/common/animate.css';
-/* 自定义图标库 */
-@import '/common/icon.css';
-/* Ui基础库 */
-@import '/common/zcm-main.css';
-/* 公共样式 */
-@import '/common/common.css';
+	/*每个页面公共css */
+	/* #ifndef APP-PLUS-NVUE */
+	/* 官方ui库 */
+	@import "/common/uni.css";
+	/* 第三方动画库 */
+	@import "/common/animate.css";
+	/* 自定义图标库 */
+	@import "/common/icon.css";
+	/* UI基础库 */
+	@import "/common/zcm-main.css";
+	/* #endif */
+	/* #ifdef APP-NVUE */
+	@import "/common/zcm-main-nvue.css";
+	/* #endif */
+	/* 公共样式 */
+	@import "/common/common.css";
 </style>
