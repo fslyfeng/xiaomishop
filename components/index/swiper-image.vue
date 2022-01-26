098a34b6a32fe4a8d3bbc8b155a8bce5a5a672c8
netren <fslyfeng@gmail.com>
2022-01-26 14:04:57 +0800

    轮播图组件二次优化

--- components/index/swiper-image.vue
+++ components/index/swiper-image.vue
@@ -1,9 +1,9 @@
 <template>
 	<view>
-		<swiper indicator-dots autoplay :interval="3000" :duration="1000" circular>
+		<swiper indicator-dots autoplay :interval="3000" :duration="1000" circular :style="getStyle">
 			<block v-for="(item, index) in resdata" :key="index">
 				<swiper-item>
-					<view class="swiper-item" @tap="event(item)"><image :src="item.src" lazy-load mode="widthFix"></image></view>
+					<view class="swiper-item" @tap="event(item, index)"><image :src="item.src" lazy-load mode="aspectFit" :style="getStyle"></image></view>
 				</swiper-item>
 			</block>
 		</swiper>
@@ -13,11 +13,33 @@
 <script>
 export default {
 	props: {
-		resdata: Array
+		resdata: Array,
+		height: {
+			type: String,
+			default: '350rpx'
+		},
+		preview: {
+			type: Boolean,
+			default: false
+		}
+	},
+	computed: {
+		getStyle() {
+			return `height:${this.height}rpx`;
+		},
+		getUrls() {
+			return this.resdata.map(v => v.src);
+		}
 	},
 	methods: {
-		event(item) {
-			console.log('点了');
+		event(item, index) {
+			if (this.preview) {
+				return uni.previewImage({
+					current: index,
+					urls: this.getUrls,
+					indicator: 'default'
+				});
+			}
 		}
 	}
 };
---
+++ pages/detail/detail.vue
@@ -0,0 +1,44 @@
+<template>
+	<view>
+		<!-- 商品详情页面 -->
+		<!-- 商品详情大图 -->
+		<swiper-image :resdata="banners" height="750" preview></swiper-image>
+	</view>
+</template>
+
+<script>
+import swiperImage from '@/components/index/swiper-image.vue';
+export default {
+	components: { swiperImage },
+	data() {
+		return {
+			banners: [
+				{
+					src: '/static/images/pic/LQ01/00001_.jpg'
+				},
+				{
+					src: '/static/images/pic/LQ01/00002_.jpg'
+				},
+				{
+					src: '/static/images/pic/LQ01/00003_.jpg'
+				},
+				{
+					src: '/static/images/pic/LQ01/00004_.jpg'
+				},
+				{
+					src: '/static/images/pic/LQ01/00005_.jpg'
+				},
+				{
+					src: '/static/images/pic/LQ01/00006_.jpg'
+				},
+				{
+					src: '/static/images/pic/LQ01/00007_.jpg'
+				}
+			]
+		};
+	},
+	methods: {}
+};
+</script>
+
+<style></style>
