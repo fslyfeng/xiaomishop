f06d584f85062427236d144a3fef0d499d73ea06
netren <fslyfeng@gmail.com>
2022-01-28 13:52:09 +0800

    自定义头部组件开发

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,22 +1,31 @@
 <template>
 	<view>
-		
+		<!-- 头部 -->
+		<view class="position-relative d-flex a-center " style="height: 320rpx;">
+			<view class="iconfont icon-xiaoxi position-absolute text-white" style="font-size: 50rpx;top: 75rpx;right:20rpx;z-index: 100;"></view>
+			<image src="../../static/images/bg.jpg" style="height: 320rpx;width: 100%;"></image>
+			<view class="d-flex a-center position-absolute left-0 right-0" style="bottom: 50rpx;">
+				<image src="../../static/images/demo/demo6.jpg" style="height: 145rpx;width: 145rpx;border: 5rpx solid;" class="rounded-circle border-light ml-4"></image>
+				<view class="ml-2 text-white font-md">用户名称</view>
+				<view
+					class="d-flex a-center j-center a-self-end ml-auto px-2"
+					style="height: 70rpx;background: #FFD43F;color: #CC4A00;border-top-left-radius: 40rpx;border-bottom-left-radius: 40rpx;"
+				>
+					<view class="line-h iconfont icon-huangguan mr-1"></view>
+					会员积份 1.99
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+export default {
+	data() {
+		return {};
+	},
+	methods: {}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
