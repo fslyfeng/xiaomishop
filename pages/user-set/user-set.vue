2d15e08d35cb49de2cf7ab9e09410e81644f661b
netren <fslyfeng@gmail.com>
2022-05-19 21:25:12 +0800

    退出登录功能

--- pages/user-set/user-set.vue
+++ pages/user-set/user-set.vue
@@ -4,12 +4,13 @@
 			<uni-list><uni-list-item :title="list.title" v-for="(list, listIndex) in item.value" link="reLaunch" :key="listIndex" :to="list.path"></uni-list-item></uni-list>
 		</card>
 
-		<view class="p-3"><button type="default" class="bg-white">退出登陆</button></view>
+		<view class="p-3"><button type="default" class="bg-white" @click="logoutEvent()">退出登录</button></view>
 	</view>
 </template>
 
 <script>
 import card from '@/components/common/card.vue';
+import { mapMutations, mapState } from 'vuex';
 export default {
 	components: {
 		card
@@ -35,7 +36,34 @@ export default {
 			]
 		};
 	},
-	methods: {}
+	computed: {
+		...mapState({
+			token: state => state.user.token,
+			loginStatus: state => state.user.loginStatus
+		})
+	},
+	methods: {
+		...mapMutations(['logout']),
+
+		//退出登录
+		logoutEvent() {
+			// this.$H.post('/logout',{},{
+			// 					token:false,
+			// 					toast:false
+			// 				}).then(res=>{
+			// 		// 退出登录
+			this.logout();
+
+			uni.showToast({
+				title: '退出成功',
+				icon: 'none'
+			});
+			uni.navigateBack({
+				delta: 1
+			});
+			//});
+		}
+	}
 };
 </script>
 
